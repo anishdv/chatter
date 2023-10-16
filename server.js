3dfc5912201f31745d7a32c9dc3b693e6b978b38
Anish Devnani <anishdev12@gmail.com>
2023-10-16 15:34:20 +0530

    socket functionality

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const app = express();
 const PORT = 5000;
 const http = require("http").createServer(app);
 
-
 app.use(express.static(__dirname + "/public"));
-
 
 app.get("/", (req, res, next) => {
   res.sendFile(__dirname + "/index.html");
 });
 http.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
+});
+
+const io = require("socket.io")(http);
+io.on("connection", (socket) => {
+  console.log("Connected...");
+  socket.on("message", (msg) => {
+    socket.broadcast.emit("message", msg);
+  });
 });
